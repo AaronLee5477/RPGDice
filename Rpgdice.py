28e2bcf5b7c706e46beb03b6877fa4d051d1212c
Aaron Lee <aandmlee@gmail.com>
2019-08-15 21:55:12 -0400

    Added a function to calculate damage roll but have not incorporated in main program

--- Rpgdice.py
+++ Rpgdice.py
@@ -1,7 +1,6 @@
 import random
 
-## simulates a d20 attack roll
-def attack_roll(number):
+def attack_roll(number): ## simulates a d20 attack roll
     base = random.randint(1, 20)
     result = base + int(number)
     if base == 1:
@@ -12,22 +11,42 @@ def attack_roll(number):
         print(str(base) + " + " + str(int(number)))
         print("You hit an Armor Class of " + str(result))
 
+def damage_roll():  ## simulates damage roll based on user input
+    total_damage = 0
+    i = 0
+    dice_size = int(input("What number-sided dice does your attack use? "))
+    number_of_dice = int(input("How many damage dice does your attack use? "))
+    damage_modifier = int(input("Do you have a damage modifier (Enter 0 if no modifier)? "))
+
+    while i < number_of_dice:
+        single_dice = random.randint(1, dice_size)
+        total_damage += single_dice
+        i += 1
+
+    total_damage += damage_modifier
+
+    print("You did " + str(total_damage) + " points of damage!!")
+
 
 while True:
+
     while True:
         attack_modifer = input("What is your Attack Modifier? ")
 
-        try:  ### Ensures valid integer input
+        try:  ## Ensures valid integer input
             val = int(attack_modifer)
             break
         except ValueError:
             print("That is not a valid modifier")
 
-
     attack_roll(attack_modifer)
 
-###  This section allows you to attack again and check for valid responses
-    while True:
+##### hit/miss prompt
+
+##### if/else to determine to run damage_roll function
+
+
+    while True:  ###  This section allows you to attack again and check for valid responses
         attack = input("Can you attack again? (Y or N): ")
         if attack.upper() == 'N' or attack.upper() == "Y":
             break
